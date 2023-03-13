8d11c707a6fe583c2dcd7c29dbb2a1794ba0f978
nickhoban <110017566+nickhoban@users.noreply.github.com>
2023-03-13 17:12:20 +0800

    Error

---
+++ script.js
@@ -0,0 +1,28 @@
+    <script>
+      
+        function say(myMessage){
+          var msg = new SpeechSynthesis(myMessage);
+          msg.text = "Hello World";
+          window.speechSynthesis.speak(msg);
+        }
+
+        /* This is a simplified version of the game.
+        The Computer ALWAYS picks ROCK.  
+        Does the user's choose win,lose, or tie vs ROCK? */
+
+        function play(user) {
+            let result = "";
+            if (user === 'rock') {
+                result = "tie";
+            }
+            if (user === 'paper') {
+                result = "win";
+            }
+            if (user === 'scissors') {
+                result = "lose";
+            }
+            var theMessage = ("Computer chose rock, you " + result + "!");
+            alert(theMessage);
+            say(theMessage);
+        }
+    </script>
